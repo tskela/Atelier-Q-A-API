5111738e94e635a4573643ac1af2476036df275f
Tea Skela <dskela@jd20.law.harvard.edu>
2021-06-23 12:34:26 -0700

    Created indexes

--- schema.sql
+++ schema.sql
@@ -1,29 +1,29 @@
 \c Q&A
 
--- CREATE TABLE questions (
---   id SERIAL PRIMARY KEY,
---   product_id INT,
---   body VARCHAR(300),
---   date_written VARCHAR(20),
---   asker_name VARCHAR(100),
---   asker_email VARCHAR(100),
---   reported BOOLEAN,
---   helpful INT
--- )
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  product_id INT,
+  body VARCHAR(300),
+  date_written VARCHAR(20),
+  asker_name VARCHAR(100),
+  asker_email VARCHAR(100),
+  reported BOOLEAN,
+  helpful INT
+);
 
--- CREATE TABLE answers (
---   id SERIAL PRIMARY KEY,
---   question_id INT,
---   body VARCHAR(300),
---   date_written VARCHAR(20),
---   answerer_name VARCHAR(100),
---   answerer_email VARCHAR(100),
---   reported BOOLEAN,
---   helpful INT,
---   CONSTRAINT question_id
---     FOREIGN KEY(question_id)
---       REFERENCES questions(id)
--- )
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY,
+  question_id INT,
+  body VARCHAR(300),
+  date_written VARCHAR(20),
+  answerer_name VARCHAR(100),
+  answerer_email VARCHAR(100),
+  reported BOOLEAN,
+  helpful INT,
+  CONSTRAINT question_id
+    FOREIGN KEY(question_id)
+      REFERENCES questions(id)
+);
 
 CREATE TABLE images (
   id SERIAL PRIMARY KEY,
@@ -32,4 +32,10 @@ CREATE TABLE images (
   CONSTRAINT answer_id
     FOREIGN KEY(answer_id)
       REFERENCES answers(id)
-)
+);
+
+CREATE INDEX ON questions (product_id);
+
+CREATE INDEX ON answers(question_id);
+
+CREATE INDEX ON images(answer_id);
