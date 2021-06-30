2dc8693ef3ed2d813ce72606bff3d6909a561db9
Tea Skela <dskela@jd20.law.harvard.edu>
2021-06-29 18:41:00 -0700

    Testing simple db queries

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.get("/loaderio-af04efb0d83370729fa4a4b8d9a2a10b.txt", (req, res) => {
   res.send("loaderio-af04efb0d83370729fa4a4b8d9a2a10b");
 });
 
+app.get("/testquery", (req, res) => {
+  client.query('SELECT * FROM test where id = 1', (err, rows) => {
+    res.status(200).send()
+  })
+})
+
 var cleanJSON = function (arr) {
   if (arr === null) {
     return [];
