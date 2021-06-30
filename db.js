154c4b10832a0f0ce0b19627d812dcc2237da83d
Tea Skela <dskela@jd20.law.harvard.edu>
2021-06-30 12:10:25 -0700

    Changed pool connection config

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ const config = require('./config.js');
 
 var connectionString = `postgresql://postgres:${config.PASSWORD}@ec2-52-53-128-252.us-west-1.compute.amazonaws.com/Q&A`;
 
-const pool = new Pool(connectionString);
+const pool = new Pool({
+  host: 'ec2-52-53-128-252.us-west-1.compute.amazonaws.com',
+  user: 'postgres',
+  database: 'Q&A',
+  password: `${config.PASSWORD}`
+});
 
 // var client = new Pool(connectionString);
 
